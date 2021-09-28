ffb91487f9e246c8841f9565a2f18dc8d93064ef
arturhgca <arturhgca@gmail.com>
2021-09-28 20:43:23 -0300

    Improve store item class

--- scrapers/item.py
+++ scrapers/item.py
@@ -4,13 +4,16 @@ from datetime import datetime
 
 @dataclass
 class StoreItem:
-    name: str
-    description: str
-    status: str
-    price: float
-    currency: str
-    store: str
-    country: str
-    url: str
-    scrape_time: datetime
-    available_date: datetime
+    name: str = None
+    description: str = None
+    status: str = None
+    price: float = None
+    currency: str = None
+    store: str = None
+    country: str = None
+    url: str = None
+    scrape_time: datetime = None
+    available_date: datetime = None
+
+    def __str__(self):
+        return '\n'.join(f'{key}: {value}' for key, value in vars(self).items() if value)
