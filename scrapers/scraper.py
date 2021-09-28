6d561a5d29b155cebe209a3fb3535839c59fad55
arturhgca <arturhgca@gmail.com>
2021-09-28 19:48:45 -0300

    Add short name method to scraper interface

--- scrapers/scraper.py
+++ scrapers/scraper.py
@@ -4,7 +4,12 @@ from typing import List
 from .item import StoreItem
 
 
-class ScraperInterface(ABC):
+class _ScraperInterface(ABC):
+    @staticmethod
+    @abstractmethod
+    def get_short_name() -> str:
+        raise NotImplementedError
+
     @abstractmethod
     def scrape(self, search_term: str) -> List[StoreItem]:
         raise NotImplementedError
