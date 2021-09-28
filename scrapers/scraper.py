3617004e9de8dd07777c1816b12ae37355aaf929
Daniel Sader <danielpontello@gmail.com>
2021-09-28 23:07:19 +0100

    Interface changes

--- scrapers/scraper.py
+++ scrapers/scraper.py
@@ -1,3 +1,10 @@
-class ScraperInterface:
-    def scrape(self, search_term: str) -> dict:
-        pass
+from abc import ABC, abstractmethod
+from typing import List
+
+from .item import StoreItem
+
+
+class ScraperInterface(ABC):
+    @abstractmethod
+    def scrape(self, search_term: str) -> List[StoreItem]:
+        raise NotImplementedError
